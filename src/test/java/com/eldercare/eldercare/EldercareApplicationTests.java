a87fa582338641b5cff8af0fc221ff526737fcce
Seth <serranoroyoseth@gmail.com>
2026-05-23 19:32:19 +0200

    Initial Spring Boot setup

---
+++ src/test/java/com/eldercare/eldercare/EldercareApplicationTests.java
@@ -0,0 +1,13 @@
+package com.eldercare.eldercare;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EldercareApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
